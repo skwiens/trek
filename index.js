ceba6b8b144c06c5fc34ee39d254cca8b8174c4d
Shaunna <skwiens@gmail.com>
2017-11-21 16:21:03 -0800

    trips disply in a table

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 $(document).ready(() => {
 
   const successCallback = (response) => {
-    console.log(response);
+    console.log(response[0]);
+    response.forEach((trip) => {
+      const $tripsTable = $('#trips');
+      const row = `
+        <tr><td>${trip.name}</td>
+        <td>${trip.continent}</td>
+        <td>${trip.weeks}</td>`;
+      console.log(row);
+      $tripsTable.append(row);
+    });
   }; // end of successCallback
 
   const url = 'https://trektravel.herokuapp.com/trips';
